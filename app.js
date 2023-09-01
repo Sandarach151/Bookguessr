f5e2ec970fd2fc10705854f2fdc73ea374788851
Sandarach151 <101811443+Sandarach151@users.noreply.github.com>
2023-09-01 10:01:29 +0000

    2nd major commit

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const uploadProcess = require("./JS/uploadProcess");
 const mergedProcess = uploadProcess.mergedProcess;
 const signup = require("./JS/signup");
 const insertUser = signup.insertUser;
+const handlePlay = require('./JS/handlePlay');
+const showPlay = handlePlay.showPlay;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -31,90 +33,45 @@ app.post('/upload', upload.single('uploaded_file'), function (req, res, next) {
     res.render('Upload.ejs');
 });
 
-app.get('/upload-checkpoint', (req, res) => {
-    res.render('UploadCheckpoint.ejs');
+app.get('/upload-checkpoint', async (req, res) => {
+    if(!req.session.user_id){
+        res.render('Outside/UploadCheckpoint.ejs');
+    }
+    else{
+        const data = await squery('SELECT * FROM users WHERE userID = ?', [req.session.user_id]);
+        const username = data[0].userName;
+        const points = data[0].userPoints;
+        res.render('Inside/UploadCheckpoint.ejs', {username, points});
+    }
 });
 
-app.get('/play', (req, res) => {
-    var curExtractID;
-    var curExtract;
-    var curBookID;
-    var curBookName;
-    var wrongBookIDs;
-    var bookAbout;
-    var curAuthorID;
-    var authorAbout;
-    var options = [];
-    squery("SELECT COUNT(*) FROM extracts;")
-        .then((data) => {
-            var numRows = data[0]['COUNT(*)'];
-            curExtractID = Math.floor(Math.random() * numRows);
-            return squery('SELECT * FROM extracts WHERE extractID = ?', [curExtractID]);
-        })
-        .then((data) => {
-            curExtract = data[0].extractText;
-            curBookID = data[0].extractBookID;
-            return squery('SELECT * FROM books WHERE bookID = ?', [curBookID]);
-        })
-        .then((data) => {
-            curBookName = data[0].bookName;
-            bookAbout = data[0].bookAbout;
-            curAuthorID = data[0].bookAuthorID;
-            return squery('SELECT COUNT(*) FROM books;');
-        })
-        .then((data) => {
-            const uniqueIntegers = new Set();
-            uniqueIntegers.add(curBookID);
-            while (uniqueIntegers.size < 6) {
-              const randomNumber = Math.floor(Math.random() * (data[0]['COUNT(*)'])) + 1;
-              uniqueIntegers.add(randomNumber);
-            }
-            wrongBookIDs = Array.from(uniqueIntegers);
-            return squery('SELECT bookName FROM books WHERE bookID = ?', [wrongBookIDs[0]]);
-        })
-        .then((data) => {
-            options.push(data[0].bookName);
-            return squery('SELECT bookName FROM books WHERE bookID = ?', [wrongBookIDs[1]]);
-        })
-        .then((data) => {
-            options.push(data[0].bookName);
-            return squery('SELECT bookName FROM books WHERE bookID = ?', [wrongBookIDs[2]]);
-        })
-        .then((data) => {
-            options.push(data[0].bookName);
-            return squery('SELECT bookName FROM books WHERE bookID = ?', [wrongBookIDs[3]]);
-        })
-        .then((data) => {
-            options.push(data[0].bookName);
-            return squery('SELECT bookName FROM books WHERE bookID = ?', [wrongBookIDs[4]]);
-        })
-        .then((data) => {
-            options.push(data[0].bookName);
-            return squery('SELECT bookName FROM books WHERE bookID = ?', [wrongBookIDs[5]]);
-        })
-        .then((data) => {
-            options.push(data[0].bookName);
-            for (let i = options.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [options[i], options[j]] = [options[j], options[i]];
-            }
-            return squery('SELECT authorAbout FROM author WHERE authorID = ?', [curAuthorID]);
-        })
-        .then((data) => {
-            authorAbout = data[0].authorAbout;
-            res.render('Play.ejs', { options, curBookName, curExtract, bookAbout, authorAbout });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+app.get('/play', async (req, res) => {
+    if(!req.session.user_id){
+        res.render('Outside/Login.ejs');
+    }
+    else{
+        const {options, curBookName, curExtract, bookAbout, authorAbout} = await showPlay();
+        const userData = await squery('SELECT * FROM users WHERE userID = ?', [req.session.user_id]);
+        const username = userData[0].userName;
+        const points = userData[0].userPoints;
+        res.render('Inside/Play.ejs', {options, curBookName, curExtract, bookAbout, authorAbout, username, points});
+    }
 });
 
-app.get('/about', (req, res) => {
-    res.render('About.ejs');
+app.get('/about', async (req, res) => {
+    if(!req.session.user_id){
+        res.render('Outside/About.ejs');
+    }
+    else{
+        const data = await squery('SELECT * FROM users WHERE userID = ?', [req.session.user_id]);
+        const username = data[0].userName;
+        const points = data[0].userPoints;
+        res.render('Inside/About.ejs', {username, points})
+    }
 })
 
 app.get('/signup', (req, res) => {
-    res.render('Signup.ejs');
+    res.render('Outside/Signup.ejs');
 })
 
 app.post('/signup', (req, res) => {
@@ -123,7 +80,7 @@ app.post('/signup', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    res.render('Login.ejs');
+    res.render('Outside/Login.ejs');
 })
 
 app.post('/login', async (req, res) => {
@@ -143,8 +100,16 @@ app.post('/login', async (req, res) => {
     }
 })
 
-app.get('/', (req, res) => {
-    res.render('MainPage.ejs');
+app.get('/', async (req, res) => {
+    if(!req.session.user_id){
+        res.render('Outside/MainPage.ejs');
+    }
+    else{
+        const data = await squery('SELECT * FROM users WHERE userID = ?', [req.session.user_id]);
+        const username = data[0].userName;
+        const points = data[0].userPoints;
+        res.render('Inside/MainPage.ejs', {username, points});
+    }
 })
 
 app.listen(process.env.PORT || 3000, () => {
