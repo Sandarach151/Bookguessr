17b9833d2c1323c4ba9df0a51a8a41d70b1d091b
Sandarach151 <101811443+Sandarach151@users.noreply.github.com>
2023-09-02 05:23:46 +0000

    final final commit

--- JS/signup.js
+++ JS/signup.js
@@ -4,7 +4,10 @@ const bcrypt = require('bcrypt');
 
 async function insertUser(username, password) {
     const hashPassword = await bcrypt.hash(password, 12);
-    await squery('INSERT INTO users (userPassword, userName, userPoints) VALUES (?, ?, ?)', [hashPassword, username, 0]);
+    const data = await squery('SELECT COUNT(*) FROM users WHERE userName = ?', [username]);
+    if(data[0]['COUNT(*)']==0){
+        await squery('INSERT INTO users (userPassword, userName, userPoints) VALUES (?, ?, ?)', [hashPassword, username, 0]);
+    }
 }
 
 exports.insertUser = insertUser;
